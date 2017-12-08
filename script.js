2a9267bb2a6459115cfcc25a6726f397fdaef30c
ARA <ARAMAMAGEI@GMAIL.com>
2017-12-08 19:25:26 +0100

    create sites for the employees

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+document.getElementById("calculate").addEventListener("click", price,false);
+
+document.getElementById("result").innerHTML= price();
+
 function price() {
 
-	var power = document.forms["costs"]["power"].value;
 	var age = document.forms["costs"]["age"].value;
+	var power = document.forms["costs"]["power"].value;
+	
 
 	var insurance = Number(power) * 100 / Number(age) + 150;
 
@@ -9,6 +14,3 @@ function price() {
 
 	}
 
-document.getElementById("calculate").addEventListener("click", price,false);
-
-document.getElementById("result").innerHTML= price();
